f904f1eb6449d246abe7b4f22950d7791b2cab30
brainsynder <briansnyder498@gmail.com>
2022-04-07 19:00:37 -0400

    Updated addon.json

--- src/main/java/addon/brainsynder/worldborder/WorldBorderAddon.java
+++ src/main/java/addon/brainsynder/worldborder/WorldBorderAddon.java
@@ -2,28 +2,13 @@ package addon.brainsynder.worldborder;
 
 import com.wimbli.WorldBorder.BorderData;
 import com.wimbli.WorldBorder.Config;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.plugin.Plugin;
 import simplepets.brainsynder.addon.presets.RegionModule;
 import simplepets.brainsynder.api.Namespace;
-import simplepets.brainsynder.api.plugin.SimplePets;
 import simplepets.brainsynder.api.user.PetUser;
-import simplepets.brainsynder.debug.DebugLevel;
 
 @Namespace(namespace = "WorldBorder")
 public class WorldBorderAddon extends RegionModule {
-
-    @Override
-    public boolean shouldEnable() {
-        Plugin worldBorder = Bukkit.getPluginManager().getPlugin("WorldBorder");
-        if (worldBorder == null) {
-            SimplePets.getDebugLogger().debug(DebugLevel.ERROR, "WorldBorder wasn't found!");
-            SimplePets.getDebugLogger().debug(DebugLevel.ERROR, "Please download it from: https://www.spigotmc.org/resources/80466/");
-            return false;
-        }
-        return true;
-    }
 
     private boolean isAllowedInRegion(PetUser petUser, Location location) {
         BorderData border = Config.Border(location.getWorld().getName());
